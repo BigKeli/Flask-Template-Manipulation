049f21e616bfe3f0da9ef0207bcc6b2d14371864
BigLali <kelvingjikola123@gmail.com>
2023-09-01 11:16:39 +0200

    pede

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ from flask import Flask, render_template, url_for
 import os
 
 app = Flask(__name__)
-doc=[]
-image=[]
+
 @app.route("/")
 def homepage():
+    doc = []
     for filename in os.listdir('static/WorkTemplates'):
 
-        name = (filename.rsplit('.docx', 1))
-        doc.append("word1")
+        size= len(filename)
+        name=filename[:size - 5]
+        doc.append(name)
     return render_template("Chooser.html",templates=doc)
 
 @app.route("/<string:TemplateName>")
 def templatePage(TemplateName):
     return render_template("TemplateView.html")
+
+def returnPDF(ToBeConverted):
+    return render_template()
