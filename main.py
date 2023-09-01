0a74ce4906690cca0d690810242ca96ce383e1b6
BigLali <kelvingjikola123@gmail.com>
2023-09-01 14:24:36 +0200

    Laert Suma

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, url_for
 import os
+from docx2pdf import convert
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
     doc = []
+    # convert("static/WorkTemplates/Contract_Temp.docx", "static/PDF/output.pdf")
+
     for filename in os.listdir('static/WorkTemplates'):
 
         size= len(filename)
@@ -15,7 +19,9 @@ def homepage():
 
 @app.route("/<string:TemplateName>")
 def templatePage(TemplateName):
-    return render_template("TemplateView.html")
+    return render_template("TemplateView.html",Template=TemplateName)
 
-def returnPDF(ToBeConverted):
-    return render_template()
+# def returnPDF(ToBeConverted):
+#     convert("static/WorkTemplates/Contract_Temp.docx", "static/PDF/output.pdf")
+#
+#     return render_template()
