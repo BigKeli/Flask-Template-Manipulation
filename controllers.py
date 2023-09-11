d6f53ce6fba7baae64ddd84b12ebc7272c405f77
Laert Huti <101653303+laerttt@users.noreply.github.com>
2023-09-11 09:49:16 +0200

    m

--- controllers.py
+++ controllers.py
@@ -1,5 +1,6 @@
 from mailmerge import MailMerge
 from datetime import date
+from docx2pdf import convert
 #'end_date', 'contract_notes',
 #'second_party_notes', 'first_party_notes',
 #'first_party', 'start_date', 'state', 'second_party', 'today_date'
@@ -15,23 +16,28 @@ def merge_Contract(end_date, contract_notes, second_party_notes,
 #'buyer_state', 'city', 'total', 'address', 'phone',
 #'quantity', 'buyer_city', 'state', 'buyer_name', 'buyer_address2',
 #'buyer_address', 'receipt_no', 'item', 'price', 'buyer_phone'
-def merge_Receipt(date, address_2, seller, total_total,
+def merge_Receipt(date, address_2, seller,
                   buyer_state, city, address, phone,
                   buyer_city, state, buyer_name,
                   buyer_address2, buyer_address, receipt_no,
                   buyer_phone, itemString):
     document = MailMerge("static/WorkTemplates/Receipt_Temp.docx")
-    document.merge(date=date, address_2=address_2, seller=seller, total_total=total_total,
+    document.merge(date=date, address_2=address_2, seller=seller,
                     buyer_state=buyer_state, city=city, address=address, phone=phone,
                     buyer_city=buyer_city, state=state, buyer_name=buyer_name,
                     buyer_address2=buyer_address2, buyer_address=buyer_address, receipt_no=receipt_no,
                     buyer_phone=buyer_phone)
     itemList = itemString.split(" ")
     sales_history = []
+    tot =0
     for i in range(0, len(itemList) - 2, 3):
         sales_history.append(dict(item=itemList[i], quantity=itemList[i + 1], unitPrice=itemList[i + 2],
                                   total=str(int(itemList[i + 2]) * int(itemList[i + 1]))))
+        tot+=int(itemList[i + 2]) * int(itemList[i + 1])
     document.merge_rows('item', sales_history)
+    test = document.merge(total_total=tot)
+    convert(test, "output.pdf")
+
 
 
 def merge_Week(end_date, name_surname, wednesday, plans_for_next_week, monday,
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, url_for
-import os
-from docx2pdf import convert
+import datetime
 
+from flask import Flask, render_template, url_for, request
+import os
+import controllers as con
+from docx2pdf import convert
+from datetime import date
 
 app = Flask(__name__)
 
@@ -17,10 +20,15 @@ def homepage():
         doc.append(name)
     return render_template("Chooser.html",templates=doc)
 
-@app.route("/<string:TemplateName>")
+@app.route("/<string:TemplateName>", methods=['GET', 'POST'])
 def templatePage(TemplateName):
-    return render_template("TemplateView.html",Template=TemplateName)
-
+    if request.method=='POST':
+        if request.form['type']==1:
+            con.merge_Receipt(datetime.date, request.form['SAddress2'],request.form['Seller'],
+            request.form['BState'], request.form['SCity'], request.form['SAddress'], request.form['SPhone'],
+            request.form['BCity'], request.form['SState'], request.form['BName'], request.form['BAddress2'],
+            0,request.form['BPhone'], request.form['Item'], '' )
+    return render_template("TemplateView.html", Template=TemplateName)
 # def returnPDF(ToBeConverted):
 #     convert("static/WorkTemplates/Contract_Temp.docx", "static/PDF/output.pdf")
 #
